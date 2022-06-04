fc629acf1247b58db88af7e3f8c854e624c5f95c
Pragati946 <pragatitiwari946@gmail.com>
2022-06-04 22:07:24 +0530

    Navbar

--- chakra-css/src/App.js
+++ chakra-css/src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 // import { Button } from '@chakra-ui/react';
 import {DrawerExample} from "./components/sidebar"
+import WithSubnavigation from "./components/Navbar"
 function App() {
   return (
     <div className="App">
-       {/* <Button colorScheme='teal' size='lg'>
-    Button
-  </Button> */}
+    
   <DrawerExample/>
+  <WithSubnavigation/>
+  <Navbar/>
     </div>
   );
 }
